3bee5ce63c3450cea603249a67b15139b43eddfe
Juan Pablo Favale <juanpablofavale@hotmail.com>
2023-02-18 23:15:01 -0300

    componentes creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './componentes/header/header.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { AcercaComponent } from './componentes/acerca/acerca.component';
+import { EstudiosComponent } from './componentes/estudios/estudios.component';
+import { HabilidadesComponent } from './componentes/habilidades/habilidades.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { ExperienciaComponent } from './componentes/experiencia/experiencia.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    AcercaComponent,
+    EstudiosComponent,
+    HabilidadesComponent,
+    ProyectosComponent,
+    ExperienciaComponent
   ],
   imports: [
     BrowserModule,
